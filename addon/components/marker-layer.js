b2e84584398716ed3db255083af0cfc8f0b79214
Eric Winton <ericwinton1@gmail.com>
2022-10-14 09:42:43 -0500

    Breaking out of icon updateFn early

--- addon/components/marker-layer.js
+++ addon/components/marker-layer.js
@@ -264,15 +264,14 @@ export default class MarkerLayer extends InteractiveLayer {
       arg: 'icon',
       // there was an old leaflet bug where draggability is lost on icon change
       updateFn(layer, value) {
-        if (layer.dragging) {
-          let enabled = layer.dragging.enabled();
-          layer.setIcon(value);
+        if (!layer.dragging) { return }
+        let enabled = layer.dragging.enabled();
+        layer.setIcon(value);
 
-          if (enabled) {
-            layer.dragging.enable();
-          } else {
-            layer.dragging.disable();
-          }
+        if (enabled) {
+          layer.dragging.enable();
+        } else {
+          layer.dragging.disable();
         }
       }
     }
